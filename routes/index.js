8b0ce339c5e311385875204aa627357bfdf2709c
vyash5075 <vyash5075@gmail.com>
2020-09-24 12:25:03 +0530

    express app

--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,8 @@ router.get('/passwordCategory', function(req, res, next) {
   res.render('password_category', { title: 'Password-Management-System' });
 });
 
+router.get('/add-new-category', function(req, res, next) {
+  res.render('addNewCategory', { title: 'Password-Management-System' });
+});
+
 module.exports = router;
