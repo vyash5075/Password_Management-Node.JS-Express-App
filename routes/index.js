c1492dbf6cbfe6b188671bd51b79c1e79d2338d2
vyash5075 <vyash5075@gmail.com>
2020-09-24 11:53:02 +0530

    express app

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Password-Management-System' });
+});
+
+router.get('/signup', function(req, res, next) {
+  res.render('signup', { title: 'Password-Management-System' });
+});
+
+router.get('/passwordCategory', function(req, res, next) {
+  res.render('password_category', { title: 'Password-Management-System' });
+});
+
+module.exports = router;
