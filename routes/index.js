49fd71a3a3206f41e267972a79595ac47f85d0b5
vyash5075 <vyash5075@gmail.com>
2020-09-27 10:36:17 +0530

    express app

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 var userModel=require('../modules/user');
 var bcrypt=require('bcryptjs');
 var jwt=require('jsonwebtoken');
+var passCatModel=require('../modules/password_category');
+var getPassCat=passCatModel.find({});
+const {check,validationResult}=require('express-validator');
 if (typeof localStorage === "undefined" || localStorage === null) {
   var LocalStorage = require('node-localstorage').LocalStorage;
   localStorage = new LocalStorage('./scratch');
@@ -127,13 +130,46 @@ else{
 });
 router.get('/passwordCategory',checkLoginUser, function(req, res, next) {
   var loginUser=localStorage.getItem('loginUser')
-  res.render('password_category', { title: 'Password-Management-System' ,loginUser:loginUser});
+  getPassCat.exec(function(err,data){
+    console.log(data)
+    if(err)throw err;
+  res.render('password_category', { title: 'Password-Management-System' ,loginUser:loginUser,records:data});
 });
+})
 
 router.get('/add-new-category',checkLoginUser, function(req, res, next) {
+
+  var loginUser=localStorage.getItem('loginUser');
+ 
+    res.render('addNewCategory', { title: 'Password-Management-System',loginUser:loginUser,errors:'',success:'' });
+  })
+  
+
+
+router.post('/add-new-category',checkLoginUser,  [ check('passwordCategory','Enter Password Category Name** ').isLength({ min: 1 })],function(req, res, next) {
   var loginUser=localStorage.getItem('loginUser')
-  res.render('addNewCategory', { title: 'Password-Management-System',loginUser:loginUser });
+  const errors=validationResult(req);
+  if(!errors.isEmpty()){
+    console.log(errors.mapped());
+    res.render('addNewCategory', { title: 'Password-Management-System',loginUser:loginUser,errors:errors.mapped(),success:''});
+
+
+  }
+  else{
+    var psscatName=req.body.passwordCategory;
+    var passcatDetails= new passCatModel({
+    passord_category:psscatName
+    });
+    passcatDetails.save(function(err,doc){
+      if(err) throw err;
+      res.render('addNewCategory', { title: 'Password-Management-System',loginUser:loginUser,errors:'',success:'successfully registered!!' });
+
+    })
+   
+  }
+  
 });
+
 
 router.get('/add-new-password', checkLoginUser,function(req, res, next) {
   var loginUser=localStorage.getItem('loginUser')
